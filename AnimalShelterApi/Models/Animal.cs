db9125a5ffc3b4a15ecbeb5449b1511f75863b88
igl-myrick <igl.myrick@gmail.com>
2025-08-28 15:58:54 -0700

    add Animal class

---
+++ AnimalShelterApi/Models/Animal.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelterApi.Models
+{
+  class Animal
+  {
+    public int AnimalId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Species { get; set; }
+    [Required]
+    public string Breed { get; set; }
+    [Required]
+    public int Age { get; set; }
+  }
+}
