f5fb539bf3e1e52d3840340b81ec701e584134b0
igl-myrick <igl.myrick@gmail.com>
2025-08-29 15:28:38 -0700

    add Login model

---
+++ AnimalShelterApi/Models/Login.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelterApi.Models
+{
+  public class Login
+  {
+    [Required]
+    [Display(Name = "Username")]
+    public string Username { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+  }
+}
