8bde081c48be91a7ace9d291b8826e4445471889
igl-myrick <igl.myrick@gmail.com>
2025-08-29 15:15:32 -0700

    add validation properties

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -6,12 +6,16 @@ namespace AnimalShelterApi.Models
   {
     public int AnimalId { get; set; }
     [Required]
+    [StringLength(20)]
     public string Name { get; set; }
     [Required]
+    [StringLength(20)]
     public string Species { get; set; }
     [Required]
+    [StringLength(40)]
     public string Breed { get; set; }
     [Required]
+    [Range(0, 50, ErrorMessage = "Age must be between 0 and 50.")]
     public int Age { get; set; }
   }
 }
