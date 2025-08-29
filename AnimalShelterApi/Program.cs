544bd2709959894b4722c5ec5bb1d2c1ef079cdd
igl-myrick <igl.myrick@gmail.com>
2025-08-29 15:37:33 -0700

    add jwt configuration

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,6 +1,9 @@
 using AnimalShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,30 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
   .AddEntityFrameworkStores<AnimalShelterApiContext>()
   .AddDefaultTokenProviders();
 
+string jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+string jwtAudience = builder.Configuration.GetSection("Jwt:Audience").Get<string>();
+string jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+  {
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidAudience = jwtAudience,
+      ValidIssuer = jwtIssuer,
+      ClockSkew = TimeSpan.Zero,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+    };
+  });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -34,6 +61,7 @@ else
     app.UseHttpsRedirection();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
