4f68fa1d7fd130d9a6d6d7de06a2802deee47b14
igl-myrick <igl.myrick@gmail.com>
2025-08-29 15:54:28 -0700

    require authorization to access controller

--- AnimalShelterApi/Controllers/AnimalsController.cs
+++ AnimalShelterApi/Controllers/AnimalsController.cs
@@ -1,4 +1,5 @@
 using AnimalShelterApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,7 @@ namespace AnimalShelterApi.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
+  [Authorize]
   public class AnimalsController : ControllerBase
   {
     private readonly AnimalShelterApiContext _db;
