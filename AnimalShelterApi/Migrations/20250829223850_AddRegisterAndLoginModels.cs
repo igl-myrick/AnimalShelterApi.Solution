e8df7be20b5780faa2c019e80a1cdd769c617f35
igl-myrick <igl.myrick@gmail.com>
2025-08-29 15:40:20 -0700

    add register and login models

---
+++ AnimalShelterApi/Migrations/20250829223850_AddRegisterAndLoginModels.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class AddRegisterAndLoginModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
