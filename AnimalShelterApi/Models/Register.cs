f6230f8987a9ef1eadf656643157685245d90fce
igl-myrick <igl.myrick@gmail.com>
2025-08-29 15:27:02 -0700

    add Register model

---
+++ AnimalShelterApi/Models/Register.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelterApi.Models
+{
+  public class Register
+  {
+    [Required]
+    [Display(Name = "Username")]
+    public string Username { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$", ErrorMessage = "Your password must contain at least six characters, a capital letter, a lowercase letter, a number, and a special character.")]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+  }
+}
